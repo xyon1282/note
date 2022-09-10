934e3ced3a583c67366551d2dc8281f3f1ea4119
xyon1282 <xyon1282@localhost.localdomain>
2022-09-09 21:34:22 -0400

    Deleted unneccesary comment and code

--- note.py
+++ note.py
@@ -11,8 +11,6 @@ parser = argparse.ArgumentParser(description='Process values for note title and 
 parser.add_argument("-t", type=str)
 # B is for body of the note
 parser.add_argument("-b", type=str)
-#F for filename. Note to self: make this optional, and if user does not specify, default to title
-#parser.add_argument("-f", type=str)
 
 args = parser.parse_args()
 
